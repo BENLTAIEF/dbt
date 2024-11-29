92aba059fdf1b9ffd2938f89da173c3f03791d5c
unknown <bechir.benltaief@gmail.com>
2024-11-29 11:02:54 +0100

    ...

--- models/dim/dim_listings_cleansed.sql
+++ models/dim/dim_listings_cleansed.sql
@@ -7,7 +7,7 @@ select listing_id
     ,room_type
     ,case when minimum_nights=0 or minimum_nights is null then 1 else minimum_nights end as minimum_nights
     ,host_id
-    ,decimal(replace(price_str, '$', '')) as price
+    ,cast(replace(price_str, '$', '') as DECIMAL(10, 2 )) as price
     ,created_at
     ,updated_at
 from src_listings

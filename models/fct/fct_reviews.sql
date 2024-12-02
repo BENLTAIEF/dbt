a7a6bbbf2ab3dd578e5c0bca90839f19b8a3571b
unknown <bechir.benltaief@gmail.com>
2024-12-02 11:21:06 +0100

    vars

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -18,5 +18,13 @@ select
 from src_reviews
 where review_text is not null
 {% if is_incremental() %}
-  and review_date >= coalesce((select max(review_date) from {{ this }}), '1900-01-01')
+    {% if var('start_date', False) and var('end_date', False) %}
+        {{ log ("Loading " ~ this ~ " incrementally (start_date: " ~ var("start_date") ~ " End Date :" ~ var("end_date") ~ " )" , info=True) }}
+        and review_date >= '{{ var("start_date") }}'
+        and review_date <  '{{ var("end_date") }}'
+    {% else %}
+        {{ log ("Loading " ~ this ~ " incrementally ( all missing dates )" , info=True) }}
+        and review_date >= coalesce((select max(review_date) from {{ this }}), '1900-01-01')    
+    {% endif %}
+  
 {% endif %}
